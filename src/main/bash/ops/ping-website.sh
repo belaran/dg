70fe78d1aee8e3a6e9e075d8a0c37f72d0b7c517
Romain Pelisse <belaran@gmail.com>
2017-12-11 00:37:43 +0100

    enhance ping

--- src/main/bash/ops/ping-website.sh
+++ src/main/bash/ops/ping-website.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly URL=${URL:-'http://main-belaran.rhcloud.com/'}
+readonly URL=${URL:-'http://wildfly-belaran.7e14.starter-us-west-2.openshiftapps.com/shadow'}
 
 readonly SMTP_SERVER='smtp.corp.redhat.com'
 readonly SMTP_PORT='587'
@@ -12,6 +12,11 @@ readonly TOPIC='Openshift Online Apps Down'
 
 readonly PROJECT_ROOT=${PROJECT_ROOT:-'/home/rpelisse/Repositories/perso/belaran.eu.git'}
 
+echo "Checking website: ${URL}" >> /tmp/website.ping.log
+curl "${URL}" 2> /dev/null > /dev/null
+sleep 2
+curl "${URL}" 2> /dev/null > /dev/null
+sleep 2
 curl -I "${URL}" 2> /dev/null | grep HTTP | grep -q '200'
 for app in dg shadow
 do
